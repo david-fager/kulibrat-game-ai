5864540adfa053a8efb005920a33dac132138d92
df <s185120@student.dtu.dk>
2023-02-28 13:47:28 +0100

    printing the board

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+board = [[" ", " ", " "],
+         [" ", " ", " "],
+         [" ", " ", " "],
+         [" ", " ", " "]]
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def userTurn():
+    ends = ["-", "-", "-"]
+    sides = np.transpose([["+", "|", "|", "|", "|", "+"]])
+    bordered = np.vstack([ends, board, ends])
+    bordered = np.hstack([sides, bordered, sides])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for line in bordered:
+        print(*line)
+
+def aiTurn():
+    print('not implemented yet')
+
+
+def gameLoop():
+    while True:
+        userTurn()
+        aiTurn()
+        break
+
+    print('Game Over')
+
+
+gameLoop()
